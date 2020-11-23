f7d1cbdc72a56a9186eb80265675813ae357af0e
DaniilKorrr <74916281+DaniilKorrr@users.noreply.github.com>
2020-11-23 17:27:26 +0300

    Create Main.java

---
+++ задача1.2/Main.java
@@ -0,0 +1,14 @@
+public static void main(String[] args){
+        int a =  3 ;
+        int b =  2 ;
+
+        //System.out.println("два");
+        System.out.println(b);
+        System.out.println("плюс");
+        //System.out.println("минус");
+        System.out.println(a);
+        //System.out.println("три");
+        System.out.println("равно");
+        //System.out.println("будет");
+        //System.out.println("пять");
+        System.out.println(a+b);
