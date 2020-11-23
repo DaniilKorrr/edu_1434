97c9ed4d459a73464d1fba2e64f701b5410b5b9d
DaniilKorrr <74916281+DaniilKorrr@users.noreply.github.com>
2020-11-23 17:28:39 +0300

    Create Main.java

---
+++ задача1.4/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+       int number = 25;
+       int a = 5;
+       int b =5;
+       int sum=(a*b);
+       
+       System.out.println(sum);
+
+    }
+
+}
